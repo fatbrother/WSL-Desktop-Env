1727b17228602fb902c81b2870c36d47bd3513a7
Fatbrother <kevin92422@gmail.com>
2023-09-03 17:09:57 +0800

    first commit

---
+++ ubuntu.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set DISPLAY=:0
+
+@REM get file path
+cd /d "%~dp0"
+
+@REM open ./config.xlaunch
+start /B /WAIT "" "./config.xlaunch"
+
+@REM run ~/gnome.sh in wsl
+start /B /WAIT "" "C:\Windows\System32\wsl.exe" -e bash -lic "cd ~ && ./gnome.sh"
+
+@REM close ./config.xlaunch
+taskkill /IM "VcXsrv.exe" /F
+
+endlocal
